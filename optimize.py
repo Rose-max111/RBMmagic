a1c8af8bc10feab3d1e4f960002f6cdb7be1fcc3
Yiming Lu <luyimingboy@163.com>
2025-03-19 17:51:46 +0800

    update

--- optimize.py
+++ optimize.py
@@ -19,5 +19,13 @@ class mcmc_optimize():
         self.n_samples = n_samples
         self.n_discard = n_discard
 
-    def stochastic_reconfiguration_H(self, qubit, tol=1e-3, lookback=5, max_iters=1000, lr=1e-1, lr_tau=None, lr_min=0.0, eps=1e-4):
+    def stochastic_reconfiguration_H(self, qubit, tol=1e-3, lookback=5, max_iters=1000, resample_phi=None, lr=1e-1, lr_tau=None, lr_min=0.0, eps=1e-4, rndkey_phi=0, rndkey_psi=1):
+        # init target model distribution and its sampler
         target_model = RBM_H_State(self.model, qubit)
+        sampler_phi = nk.sampler.MetropolisLocal(
+            self.hilbert, n_chains=self.n_chains, sweep_size=self.n_sweeps)
+        sampler_phi_state = sampler_phi.init_state(
+            target_model, 1, jax.random.key(rndkey_phi))
+        # sample from the target model distribution
+        samples_phi, sampler_phi_state = sampler_phi.sample(
+            target_model, 1, state=sampler_phi_state, chain_length=self.n_samples)
